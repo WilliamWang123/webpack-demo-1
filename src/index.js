63256d8d276d88d0d314a8727bbbf6e4a1f534db
hanzo-genji <chenjinjin233@outlook.com>
2020-06-18 13:27:28 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import x from './x.js'
 import png from './assets/1.png'
 console.log(png)
-// const div = document.getElementById('app')
+const div = document.getElementById('app')
 // console.log('div')
 // console.log(div)
 // div.innerHTML = `
